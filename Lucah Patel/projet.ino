a6fec98eb2cbacbc501ecf845369904f19ae1604
Lucah Patel <134391638+bogwee@users.noreply.github.com>
2024-01-24 14:20:21 +0100

    Update projet.ino
    aller a droite, gauche et arreter par bleutooth

--- Lucah Patel/projet.ino
+++ Lucah Patel/projet.ino
@@ -1,16 +1,18 @@
 #include<SoftwareSerial.h>
 
 #define TX 11
-#define RX 107
-SoftwareSerial RC(RX, TX);
+#define RX 10
+SoftwareSerial BC(RX, TX);
 int PWM;
 char DATA;
 // Moteur A
-#define IN1 3
-#define IN2 5
+#define IN1 3  //IN1 fait avancer
+#define IN2 5  //IN2 fait reculer
 // Moteur B
-#define IN3 6
-#define IN4 9
+#define IN3 6  //IN3 fait avancer
+#define IN4 9  //IN4 fait reculer
+
+//Les moteurs sont asymetriques dons pour avancer le module il faut que le moteur A avance et le moteur B recule
 
 void setup() {
   Serial.begin(9600);
@@ -18,21 +20,38 @@ void setup() {
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-  RC.begin(9600);
+  BC.begin(9600);
 }
 
 // valeur du moteur compris entre 0 (tourne lentement) et 255(tourne rapidement)
-
-
+  
 void loop() {
-  if (RC.available()) {
-    DATA = RC.read();
+  if (BC.available()) {
+    DATA = BC.read();
     if (DATA == 'F') {
-      PWM = RC.parseInt();
+      PWM = BC.parseInt();
       Serial.println(PWM);
       analogWrite(IN1, PWM);
       analogWrite(IN2, 0);
-      analogWrite(IN3, PWM);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, PWM);
+    }
+    if (DATA == 'R') {
+      analogWrite(IN1, 240);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, 0);
+    }
+    if (DATA == 'L') {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, 240);
+    }
+    if (DATA == 'S') {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, 0);
       analogWrite(IN4, 0);
     }
   }
