08e6a7588e29d481ff8ac5f9a27efade16b027e4
Lucah Patel <134391638+bogwee@users.noreply.github.com>
2024-02-22 18:30:53 +0100

    Update tube.ino
    update

--- Lucah Patel/tube.ino
+++ Lucah Patel/tube.ino
@@ -1,23 +1,100 @@
-#define MG1 4
-#define MD1 5
-#define MG2 9
-#define MD2 3
+#include<SoftwareSerial.h>
+#include<Servo.h>
+
+#define TX 1
+#define RX 0
+
+SoftwareSerial BC(RX, TX);
+
+Servo servo1;
+Servo servo2;
+
+int PWM;
+int ANG;
+char DATA;
+
+// Moteur A
+#define IN1 11
+#define IN2 10
+// Moteur B
+#define IN3 5
+#define IN4 3
+
+// Tube
+#define T1 6
+#define T2 9
 
 void setup() {
   Serial.begin(9600);
-  pinMode(MG1, INPUT);
-  pinMode(MD1, INPUT);
-  pinMode(MG2, INPUT);
-  pinMode(MD2, INPUT);
-  analogWrite(MG1, 0);
-  analogWrite(MD1, 0);
-  analogWrite(MG2, 0);
-  analogWrite(MD2, 0);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  servo1.attach(2);
+  servo2.attach(4);
+  BC.begin(9600);
 }
 
 void loop() {
-  analogWrite(MG1, 240);
-  analogWrite(MD1, 0);
-  analogWrite(MG2, 0);
-  analogWrite(MD2, 240);
+  if (BC.available()) {
+    DATA = BC.read();
+    if (DATA == 'F') {
+      analogWrite(IN1, 255);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, 255);
+    }
+    if (DATA == 'R') {
+      analogWrite(IN1, 255);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, 0);
+    }
+    if (DATA == 'L') {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, 255);
+    }
+    if (DATA == 'S') {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, 0);
+    }
+    if (DATA == 'D') {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, 255);
+      analogWrite(IN3, 0);
+      analogWrite(IN4, 0);
+    }
+    if (DATA == 'G') {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, 0);
+      analogWrite(IN3, 255);
+      analogWrite(IN4, 0);
+    }
+    if (DATA == 'B') {
+      analogWrite(IN1, 0);
+      analogWrite(IN2, 255);
+      analogWrite(IN3, 255);
+      analogWrite(IN4, 0);
+    }
+    if (DATA == 'Z') {
+      ANG = BC.parseInt();
+      servo1.write(ANG);
+    }
+    if (DATA == 'X') {
+      ANG = BC.parseInt();
+      servo2.write(ANG);
+    }
+    if (DATA == 'K') {
+      analogWrite(T1, 255);
+      analogWrite(T2, 255);
+    }
+    if (DATA == 'E') {
+      analogWrite(T1, 0);
+      analogWrite(T2, 0);
+    }
+  }
 }
